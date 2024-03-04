f63918d09fc9e8e247f1b80540a702b69182c1e8
todo <vudv@ominext.com>
2024-03-04 10:50:30 +0700

    daily commits

--- apps/nuxt.config.ts
+++ apps/nuxt.config.ts
@@ -1,8 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
+
+
+export default ({
+
+  head: {
+    title: 'apps',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
   css: ["bootstrap/dist/css/bootstrap.css"],
   plugins: [
     { src: '~/plugins/bootstrap.ts', mode: 'client' }
   ],
+
+  components: true,
+
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build'
+  ],
+
+  // devtools: { enabled: true },
 });
